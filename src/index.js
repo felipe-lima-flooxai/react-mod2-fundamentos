072c42379d626fc37d81968e3e5dd32c965e0283
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-04-10 12:11:00 -0300

    feat: initial repository files
    Pasta criada com create-react-app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+import React from "react";
+
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+
+  <h1>Olá React</h1>
+
+);
