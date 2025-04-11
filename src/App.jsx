2a8d3ae3dec12ecd19fe87e81b3abcd3399df9c9
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-04-11 11:05:43 -0300

    feat: adicionando cores dinamicas aos cards

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,38 @@ import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
 import Aleatorio from "./components/basicos/Aleatorio";
 import Card from "./components/layout/Card";
+import "./App.css";
 
 
 
 export default function App(props){
 
 return (
-    <div id="app">
+    <div className="App">
         
-        
-        <Card titulo="Desafio Aleatório">
-            <Primeiro></Primeiro>
-        </Card>
-        
-        <Card titulo="Desafio Aleatório">
-            <ComParametro titulo="Segundo Componente" subtitulo="Muito Legal!"></ComParametro>
-        </Card>
+        <h1>Fundamentos React</h1>
 
-        <Card titulo="Desafio Aleatório">
-            <Fragmento></Fragmento>
-        </Card>
+        <div className="Cards">
+
+            <Card titulo="Primeiro Card" color="#FA6900">
+                <Primeiro></Primeiro>
+            </Card>
+            
+            <Card titulo="Card Com parâmetro" color="#E94C6F">
+                <ComParametro titulo="Segundo Componente" subtitulo="Muito Legal!"></ComParametro>
+            </Card>
+
+            <Card titulo="Card envolvido com fragmento" color="#E8B71A">
+                <Fragmento></Fragmento>
+            </Card>
+            
+            <Card titulo="Desafio Aleatório" color="#588C73">
+                <Aleatorio numeroMin = {10} numeroMax = {20}></Aleatorio>
+            </Card>
+
+        </div>
+
         
-        <Card titulo="Desafio Aleatório">
-            <Aleatorio numeroMin = {10} numeroMax = {20}></Aleatorio>
-        </Card>
     </div>
 )
 }
--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -4,10 +4,14 @@ import "./Card.css"
 
 export default props =>{
 
+    const cardStyle = {
+        backgroundColor: props.color || "#F00",
+        borderColor: props.color || "0F00"
+    }
 
     return (
 
-        <div className="Card">
+        <div className="Card" style={cardStyle}>
             <div className="Title">{props.titulo}</div>
             <div className="Content">
                 {props.children}
